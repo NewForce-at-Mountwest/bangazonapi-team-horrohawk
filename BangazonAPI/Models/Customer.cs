117151c518d9adc34561322fb77735d6caff3df7
Newforce44 <allancook@crownmkg.com>
2019-12-09 13:25:15 -0500

    models created and dotnet refresh ran on .csproj files

---
+++ BangazonAPI/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public DateTime AccountCreated { get; }
+        public DateTime lastActive { get; }
+
+        public List<PaymentType> PaymentType { get; set; } = new List<PaymentType>
+            public List<Product> ProductsForSale { get; set; } = new List<Product>
+    }
+}
