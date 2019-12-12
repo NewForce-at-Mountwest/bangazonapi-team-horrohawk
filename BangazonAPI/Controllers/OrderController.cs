35269418bb91a1f44a2432d129aa51432d941167
Daryl Adkins <daryl.adkins@gmail.com>
2019-12-12 11:21:15 -0500

    full crud and integration testing for ticket 4 functioning

--- BangazonAPI/Controllers/OrderController.cs
+++ BangazonAPI/Controllers/OrderController.cs
@@ -29,43 +29,74 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        //Code for getting a list of instructors
+        //Code for getting a list of orders
 
-        // GET: api/StudentExercisesAPI
+        // GET: api/BangazonAPI
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get(string completed, string q)
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
+                                                          
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = $@"SELECT Id, CustomerId, PaymentTypeId
-                                           FROM [Order]";
+                    string command = "";
 
+                    //Should be able to filter out completed orders with the ? completed = false query string parameter.
+
+                    if (completed == "true")
+                    {
+                        string ordersCompleted = @"SELECT Id, CustomerId, PaymentTypeId                                     
+                       FROM [Order] WHERE PaymentTypeId IS NOT NULL";
+
+
+                        command = ordersCompleted;
+
+                    }
+                    else if (completed == "false")
+                    {
+                        string ordersNotCompleted = $@"SELECT Id, CustomerId, PaymentTypeId                                     
+                        FROM [Order] WHERE PaymentTypeId IS NULL";
+
+                        command = ordersNotCompleted;
+                    }
+                    else
+                    {
+                        command = $@"SELECT Id, CustomerId, PaymentTypeId                                     
+                       FROM [Order]";
+
+                      
+                    }
+
+
+                    cmd.CommandText = command;
                     SqlDataReader reader = cmd.ExecuteReader();
                     List<Order> orders = new List<Order>();
 
                     while (reader.Read())
                     {
-                        Order order = new Order
+
+                        Order currentOrder = new Order
                         {
                             id = reader.GetInt32(reader.GetOrdinal("Id")),
                             customerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
                             paymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId")),
-                           
                         };
-
-                        orders.Add(order);
+                        orders.Add(currentOrder);
                     }
+                    
                     reader.Close();
 
                     return Ok(orders);
+                    
                 }
             }
         }
 
-        // GET: api/Order/5
+                               
+
+            // GET: api/Order/5
         [HttpGet("{id}", Name = "GetOrder")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
@@ -92,6 +123,9 @@ namespace BangazonAPI.Controllers
 
                         };
                     }
+
+
+
                     reader.Close();
 
                     return Ok(order);
@@ -164,9 +198,66 @@ namespace BangazonAPI.Controllers
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
+
         {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                        {                        
+                        cmd.CommandText = @"SELECT OrderId
+                                          FROM OrderProduct 
+                                          WHERE OrderId = @id";
+
+                       
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                       
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        
+                        if (reader.Read())
+                        {
+                            cmd.CommandText = @"DELETE FROM OrderProduct WHERE OrderId = @id1";
+                            cmd.Parameters.Add(new SqlParameter("@id1", id));
+
+                            cmd.CommandText += @" DELETE FROM [Order] WHERE Id = @id2";
+                            cmd.Parameters.Add(new SqlParameter("@id2", id));
+                        }
+                        else
+                        {
+                            cmd.CommandText = @"DELETE FROM [Order] WHERE Id = @id3";
+                            cmd.Parameters.Add(new SqlParameter("@id3", id));
+                        }
+                        reader.Close();
+
+                        //throw new Exception("No rows affected");
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+
+
+                    }                  
+                }
+            }
+            catch (Exception)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
+       
 
         private bool OrderExists(int id)
         {
@@ -175,10 +266,10 @@ namespace BangazonAPI.Controllers
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = 
-                       $@"SELECT Id, CustomerId, PaymentTypeId
-                                           FROM[Order]                       
-                                           WHERE Id = @id";
+                    cmd.CommandText =  @"SELECT Id, CustomerId, PaymentTypeId
+                                         FROM [Order]
+                                         WHERE Id = @id";
+                                                             
                     cmd.Parameters.Add(new SqlParameter("@id", id));
 
                     SqlDataReader reader = cmd.ExecuteReader();
@@ -190,23 +281,3 @@ namespace BangazonAPI.Controllers
 }
 
 
-//Verbs to be supported
-
-//GET
-//POST
-//PUT
-//DELETE
-//User should be able to GET a list, and GET a single item.
-//When an order is deleted, every line item (i.e.entry in OrderProduct) should be removed
-
-//Should be able to filter out completed orders with the ? completed = false query string parameter.
-
-//  If the parameter value is true, then only completed order should be returned.
-
-//  If the query string parameter of? _include = products is in the URL, then the list of products in the order should be returned.
-
-//  If the query string parameter of? _include = customers is in the URL, then the customer representation should be included in the response.
-
-
-//  Testing Criteria
-//  Write a testing class and test methods that validate the GET, POST, PUT, and DELETE operations work as expected.
